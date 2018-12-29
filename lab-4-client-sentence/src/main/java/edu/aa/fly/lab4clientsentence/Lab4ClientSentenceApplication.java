3da7ed382e3d28a9b7f2f942809f5b6ab5896e3a
Prasad Gamini <>
2018-12-28 23:39:54 -0600

    Changes to sentence

--- lab-4-client-sentence/src/main/java/edu/aa/fly/lab4clientsentence/Lab4ClientSentenceApplication.java
+++ lab-4-client-sentence/src/main/java/edu/aa/fly/lab4clientsentence/Lab4ClientSentenceApplication.java
@@ -2,13 +2,14 @@ package edu.aa.fly.lab4clientsentence;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "edu.aa.fly")
 public class Lab4ClientSentenceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab4ClientSentenceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab4ClientSentenceApplication.class, args);
+    }
 
 }
-
