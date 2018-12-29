8872ba66488ca7499d88a591ec2c43ca56a6c28b
Prasad Gamini <>
2018-12-29 15:41:12 -0600

    spring-cloud-ribbon aware

--- lab-4-client-sentence/src/main/java/edu/aa/fly/lab4clientsentence/controller/SentenceController.java
+++ lab-4-client-sentence/src/main/java/edu/aa/fly/lab4clientsentence/controller/SentenceController.java
@@ -3,6 +3,7 @@ package edu.aa.fly.lab4clientsentence.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,13 +17,16 @@ public class SentenceController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/sentence")
     public String getSentence() {
-        return getWord("LAB-4-CLIENT-SUBJECT") + " "
-                + getWord("LAB-4-CLIENT-VERB") + " "
-                + getWord("LAB-4-CLIENT-ARTICLE") + " "
-                + getWord("LAB-4-CLIENT-ADJECTIVE") + " "
-                + getWord("LAB-4-CLIENT-NOUN") + ".";
+        return getWordWithRibbon("LAB-4-CLIENT-SUBJECT") + " "
+                + getWordWithRibbon("LAB-4-CLIENT-VERB") + " "
+                + getWordWithRibbon("LAB-4-CLIENT-ARTICLE") + " "
+                + getWordWithRibbon("LAB-4-CLIENT-ADJECTIVE") + " "
+                + getWordWithRibbon("LAB-4-CLIENT-NOUN") + ".";
     }
 
     public String getWord(String service) {
@@ -32,6 +36,16 @@ public class SentenceController {
             if (uri != null) {
                 return (new RestTemplate()).getForObject(uri, String.class);
             }
+        }
+        return null;
+    }
+
+    public String getWordWithRibbon(String service) {
+        ServiceInstance serviceInstance = loadBalancerClient.choose(service);
+
+        URI uri = serviceInstance.getUri();
+        if (uri != null) {
+            return (new RestTemplate()).getForObject(uri, String.class);
         }
         return null;
     }
