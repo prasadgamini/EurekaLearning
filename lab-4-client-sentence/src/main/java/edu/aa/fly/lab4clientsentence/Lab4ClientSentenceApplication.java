8711ee471c25a7b0d8c1c6bf430c94336871d4f7
Prasad Gamini <>
2018-12-28 23:25:25 -0600

    Adding sentence project

---
+++ lab-4-client-sentence/src/main/java/edu/aa/fly/lab4clientsentence/Lab4ClientSentenceApplication.java
@@ -0,0 +1,14 @@
+package edu.aa.fly.lab4clientsentence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab4ClientSentenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4ClientSentenceApplication.class, args);
+	}
+
+}
+
